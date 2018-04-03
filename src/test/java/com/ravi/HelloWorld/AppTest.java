7c49352fea37acd2e32aef688004e45f16ff0db2
ravindra <ravipatidar54@gmail.com>
2018-04-03 16:00:35 +0300

    First check in

---
+++ src/test/java/com/ravi/HelloWorld/AppTest.java
@@ -0,0 +1,48 @@
+package com.ravi.HelloWorld;
+
+import java.io.FileWriter;
+import java.util.List;
+import java.util.Map;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+    
+	
+	public void testHelloWorld() throws Exception
+	{
+		System.out.println( "Hello World From Test!" );
+		assertTrue( true );	
+	}
+}
